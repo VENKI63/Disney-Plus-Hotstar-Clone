99b3ab66cf9031b6b49878243defd8267bc52abb
Venkatesh Mani <37432460+VENKI63@users.noreply.github.com>
2022-03-27 11:25:46 +0530

    Add files via upload

---
+++ movie.js
@@ -0,0 +1,27 @@
+let movies = [
+    {
+        name: '83',
+        des: 'Chroniciling the Historic 1983 World cup,Thirteen undergoes led by Kapil Dev fight and win against all odds to put on the map',
+        image: 'https://img1.hotstarext.com/image/upload/f_auto,t_web_m_1x/sources/r1/cms/prod/5603/1145603-h-3038c1e973f7'
+    },
+    {
+        name: 'Ghum Kai Kisikey pyaar Meiin',
+        des: 'Going beyond the chains of duty, Virat is slowly falling in love with Sai. But will Pakhi too let go of his broken promises?.',
+        image: 'https://img1.hotstarext.com/image/upload/f_auto,t_web_m_1x/sources/r1/cms/prod/3006/1073006-h-a2616aa41c91'
+    },
+    {
+        name: 'CSK VS KKR',
+        des: 'Tata IPL 2022 Chennai Super kings Vs Kolkata Knight Riders.Here is the Match preview',
+        image: 'https://cricketaddictor.com/wp-content/uploads/2022/03/1-1.png'
+    },
+    {
+        name: 'Chhichhore',
+        des: 'Divided by time, united by a tragic incident. In a bittersweet reunion, seven middle-aged friends ta ...',
+        image: 'https://img1.hotstarext.com/image/upload/f_auto,t_web_m_1x/sources/r1/cms/prod/1331/641331-h'
+    },
+    {
+        name: 'Deadpool',
+        des: 'Wade Wilson life is changed forever when an experiment by twisted scientist Ajax goes wrong With.',
+        image: 'https://staticg.sportskeeda.com/editor/2022/01/74d3c-16426630332228-1920.jpg'
+    }
+]
